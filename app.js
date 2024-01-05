bfef46e8b0da8934d543a8f43fd608fb2f45f935
Satyendra Upadhyay <satyendraupadya49@gmail.com>
2024-01-05 19:46:22 +0530

    2nd Commit

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log("Satyendra");
+    res.setHeader('Content-Type', 'text/html');
 
-})
+    const url = req.url;
+    if (url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>About Us</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Node.js Project</title></head>');
+        res.write('<body><h1>Welcome to my Node.js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.write('<html>');
+        res.write('<head><title>Default Page</title></head>');
+        res.write('<body><h1>Hello from nodejs server</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+});
 
-server.listen(4000);
+server.listen(3000);
